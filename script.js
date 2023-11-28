6d8e0dd297da586bf5c965fcebc087a62b5744c5
Piyush Jain <126599802+piyushjain5350@users.noreply.github.com>
2023-11-28 23:10:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+const result = window.confirm("Do you want to proceed?");
+
+if(result){
+    alert("You clicked OK. Proceeding...")
+}
+else{
+    alert("You clicked Cancel. Exiting...");
+}
